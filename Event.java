f2d164f756131fe521eecc89da8de10d169eed8e
daglenewinkel <47395528+daglenewinkel@users.noreply.github.com>
2019-04-05 11:33:11 -0500

    Add files via upload

--- Event.java
+++ Event.java
@@ -11,6 +11,12 @@ public class Event implements Comparable<Event>, Serializable
     private String startTime;
     private String endTime;
 
+    /**Creates an event object.
+     * @param eventTitle
+     * @param date
+     * @param startTime
+     * @param endTime
+     */
     public Event(String eventTitle, String date,
                  String startTime, String endTime)
     {
@@ -19,47 +25,75 @@ public class Event implements Comparable<Event>, Serializable
         this.startTime = startTime;
         this.endTime = endTime;
     }
-
+    
+    /** Returns the title of the event
+     * @return eventTitle
+     */
     public String getEventTitle()
     {
         return eventTitle;
     }
-
+    
+    /** Returns the date of the event
+     * @return date
+     */
     public String getDate()
     {
         return date;
     }
 
+    /** Returns the start time of the event
+     * @return startTime
+     */
     public String getStartTime()
     {
         return startTime;
     }
 
+    /** Returns the end time of the event. 
+     * @return endTime
+     */
     public String getEndTime()
     {
         return endTime;
     }
 
+    /** Updates the title of an event
+     * @param title
+     */
     public void setEventTitle(String title)
     {
         eventTitle = title;
     }
 
+    /** Updates the date of the event
+     * @param newDate
+     */
     public void setDate(String newDate)
     {
         date = newDate;
     }
-
+    
+    /** Updates the start time of the event
+     * @param time
+     */
     public void setStartTime(String time)
     {
         startTime = time;
     }
 
+    /** Updates the end time of the event.
+     * @param time
+     */
     public void setEndTime(String time)
     {
         endTime = time;
     }
 
+    /** Determines if two event objects are equal
+     * @param obj
+     * @return result
+     */
     public boolean equals(Object obj)
     {
         Event other;
@@ -77,6 +111,10 @@ public class Event implements Comparable<Event>, Serializable
         return result;
     }
 
+    /** Compares two event objects
+     * @param other
+     * @return result
+     */
     public int compareTo(Event other)
     {
         int result = eventTitle.compareTo(other.eventTitle);
@@ -90,6 +128,9 @@ public class Event implements Comparable<Event>, Serializable
         return result;
     }
 
+    /** Converts event object into string
+     * @return String
+     */
     public String toString()
     {
         return (eventTitle + " - " + date +

76dbb8c081da7796366afd4263515ddb96bfca18
seandersontlu <47400693+seandersontlu@users.noreply.github.com>
2019-05-05 15:20:09 -0500

    Add files via upload
    Updated to include the price of the event

---
+++ EventTreeSet.java
@@ -0,0 +1,41 @@
+// Montrell Wiley, David Glenewinkel, Sarah Anderson
+// CSCI 434, Project #2
+// 4/1/19
+
+import java.util.*;
+import java.io.*;
+
+public class EventTreeSet
+{
+    public static void main(String[] args)
+    {
+        final String FILE_NAME = "events.csv";
+        TreeSet<Event> eventSet = new TreeSet<Event>();
+
+        try (Scanner fileScan = new Scanner(new File(FILE_NAME)))
+        {
+            while (fileScan.hasNext())
+            {
+                String line = fileScan.nextLine();
+                String[] items = line.split(",");
+
+                String eventTitle = items[0];
+                String date = items[1];
+                String startTime = items[2];
+                String endTime = items[3];
+                double facePrice = Double.parseDouble(items[4]);
+                eventSet.add(new Event(
+                    eventTitle, date, startTime, endTime, facePrice));
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("Cannot open file.");
+        }
+
+        Iterator<Event> iter = eventSet.iterator();
+
+        while (iter.hasNext())
+            System.out.println(iter.next());                     
+    }
+}
